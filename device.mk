8cc642304587fdf82a08913a43131dd2a2dd0a3d
Scott Warner <Tortel1210@gmail.com>
2017-02-19 19:47:45 -0500

    x2: Include sensor conf
    Change-Id: I24467048893c150445b176fc75a52556e1870d10

--- device.mk
+++ device.mk
@@ -63,5 +63,10 @@ PRODUCT_PACKAGES += \
     qmi_config.xml \
 
 
+# Sensors
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/sensors/hals.conf:system/etc/sensors/hals.conf \
+    $(LOCAL_PATH)/sensors/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf
+
 # msm8996-common
 $(call inherit-product, device/leeco/msm8996-common/msm8996.mk)
