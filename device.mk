0623f45e3f80de7b60229397d73debad1aab6277
andr68rus <ashtmb@gmail.com>
2017-11-12 12:42:49 +0300

    x2: move sensor configs to vendor
    Change-Id: I7d89dfa690b9b03e84605fd309f7f415ba374e98

--- device.mk
+++ device.mk
@@ -65,8 +65,8 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/sensors/hals.conf:system/etc/sensors/hals.conf \
-    $(LOCAL_PATH)/sensors/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf
+    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
+    $(LOCAL_PATH)/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
 
 # msm8996-common
 $(call inherit-product, device/leeco/msm8996-common/msm8996.mk)
