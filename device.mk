93692a688cce1091bedf51f8600718725ac05c0f
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-11-12 12:57:21 +0300

    x2: move Lineage specific overlays to overlay-lineage
    Change-Id: I6b5f55fa364e49d534fbffe5e70cdf614066e1ae

--- device.mk
+++ device.mk
@@ -24,7 +24,9 @@
 $(call inherit-product-if-exists, vendor/leeco/x2/x2-vendor.mk)
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+#    $(LOCAL_PATH)/overlay-lineage
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
