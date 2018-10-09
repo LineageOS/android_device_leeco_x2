1f174b7179505415b498bb5b9b589bc83b339fc3
Scott Warner <Tortel1210@gmail.com>
2018-10-09 16:09:41 -0400

    x2: Add devicesettings for controlling camera focus fix
    Change-Id: Ibd5b1aa35c9924b14c4731306773b4973fea5ddf

--- device.mk
+++ device.mk
@@ -71,5 +71,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
     $(LOCAL_PATH)/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
 
+PRODUCT_PACKAGES += \
+    DeviceSettings
+
 # msm8996-common
 $(call inherit-product, device/leeco/msm8996-common/msm8996.mk)
