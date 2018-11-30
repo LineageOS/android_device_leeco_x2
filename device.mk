089686d3e75a04532e4b426e2bfec0bb9627e83a
Abhishek Aggarwal <warabhishek@gmail.com>
2018-11-30 00:01:12 -0700

    x2: Generate thermal-engine.conf
    * adb shell thermal-engine –o > thermal-engine.conf
    
    Change-Id: I2845e7b8bf8ea2e8aac64bb12813a320de6cebcd

--- device.mk
+++ device.mk
@@ -80,6 +80,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     tinyplay
 
+# Thermal-engine
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+
 # Vendor properties
 -include $(LOCAL_PATH)/vendor_prop.mk
 
