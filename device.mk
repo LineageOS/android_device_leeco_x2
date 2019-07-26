355400461ad890b65ae0fb4b9e85c2327019a28e
mosimchah <mosimchah@gmail.com>
2019-07-26 19:48:36 -0400

    x2: commonize thermal-engine.conf
    Change-Id: I1887fc7de7534574a0592fb717dfe8b25c87ad36

--- device.mk
+++ device.mk
@@ -73,10 +73,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     tinyplay
 
-# Thermal-engine
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
-
 # Vendor properties
 -include $(LOCAL_PATH)/vendor_prop.mk
 
