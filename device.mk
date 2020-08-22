36cf40db0be78d255ad271d03c47029c0b1cb372
Marc Bourgoin <themard85@gmail.com>
2020-08-22 00:53:40 -0600

    x2: Remove dalvik values
    We are switching to init detects memory value and sets dalvik
    accordingly
    
    Change-Id: If5e0e96be974b4a0a08fa53b8b9c8cacb3c4c66d

--- device.mk
+++ device.mk
@@ -49,15 +49,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.device.rc
 
-# Dalvik
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=16m \
-    dalvik.vm.heapgrowthlimit=256m \
-    dalvik.vm.heapsize=512m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=4m \
-    dalvik.vm.heapmaxfree=16m
-
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service
